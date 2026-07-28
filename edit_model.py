996ec71812d33d7d05ba2bd6c06cec3d6887f178
arushbisht12 <arush.bijay.bisht@gmail.com>
2026-07-28 11:20:01 -0700

    2 link manipulator with end effector sensor.

---
+++ edit_model.py
@@ -0,0 +1,27 @@
+import os 
+import mujoco as mj
+import mujoco.viewer
+
+# Get absolute path to XML file
+XML_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "manipulator.xml"))
+
+def load_model():
+    if not os.path.exists(XML_PATH):
+        raise FileNotFoundError(f"XML file not found at: {XML_PATH}")
+    m = mujoco.MjModel.from_xml_path(XML_PATH)
+    d = mujoco.MjData(m)
+    return m, d
+
+def main():
+
+    d.qpos[0] = 0
+    d.qpos[1] = 1.57
+
+    print("Launching MuJoCo Interactive Viewer...")
+    mujoco.viewer.launch(loader=load_model)
+
+if __name__ == "__main__":
+    main()
+
+        
+    
--- main.py
+++ main.py
@@ -1,26 +1,65 @@
+import os
 import time
-import mujoco
+import mujoco as mj
 import mujoco.viewer
+
 import numpy as np
 
-def main():
+# Get absolute path to XML file
+XML_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "manipulator.xml"))
+
+def load_model():
+    if not os.path.exists(XML_PATH):
+        raise FileNotFoundError(f"XML file not found at: {XML_PATH}")
     
-    m = mujoco.MjModel.from_xml_path("mjcf.xml")
+    return m, d
+
+def main():
+    m = mujoco.MjModel.from_xml_path(XML_PATH)
     d = mujoco.MjData(m)
 
+    camera_name = "my_cam"
+    camera_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_CAMERA, camera_name)
+
+    cam = mujoco.MjvCamera()
+    cam.type = mujoco.mjtCamera.mjCAMERA_FIXED
+    cam.fixedcamid = camera_id
+
     print("Launching MuJoCo Interactive Viewer...")
-    print("Press the Spacebar to pause/unpause the physics. Press ESC to exit.")
-    
+    print(f"Loading model from: {XML_PATH}")
+
     with mujoco.viewer.launch_passive(m, d) as viewer:
-        start_time = time.time()
+        viewer.cam.type = cam.type
+        viewer.cam.fixedcamid = cam.fixedcamid
+
+        N = 1000
+        q0_start = 0
+        q0_end = 1.57
+        q1_start = 0
+        q1_end = 1.57
+        q0 = np.linspace(q0_start, q0_end, N)
+        q1 = np.linspace(q1_start, q1_end, N)
         
-        while viewer.is_running() and time.time() - start_time < 30.0:
+        #initialize
+        d.qpos[0] = q0_start
+        d.qpos[1] = q1_start
+
+        i = 0
+
+        while viewer.is_running():
             step_start = time.time()
-            
-            mujoco.mj_step(m, d)
+
+            if i < N:
+                d.qpos[0] = q0[i]
+                d.qpos[1] = q1[i]
+                mj.mj_forward(m, d)
+                i += 1
+
+            print(d.site_xpos[0])
+
             
             viewer.sync()
-            
+
             time_until_next_step = m.opt.timestep - (time.time() - step_start)
             if time_until_next_step > 0:
                 time.sleep(time_until_next_step)
