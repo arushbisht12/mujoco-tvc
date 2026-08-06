2819d31f9fad2fde8223f52f03cf6ba8e8063ff5
arushbisht12 <arush.bijay.bisht@gmail.com>
2026-08-05 17:41:55 -0700

    LQR controller for underactuated 2 link pendulum.

--- manipulator.py
+++ manipulator.py
@@ -7,11 +7,12 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import control
 
 # Get absolute path to XML file
-XML_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "manipulator.xml"))
+XML_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "2D_double_pendulum.xml"))
 
-q0_init = -3 * np.pi/2
+"""q0_init = -3 * np.pi/2
 q1_init = 0
 q0_end = -np.pi/2
 q1_end = np.pi/2
@@ -23,13 +24,39 @@ t = []
 qact0 = []
 qref0 = []
 qact1 = []
-qref1 = []
+qref1 = []"""
 
 def init_controller(m, d):
     global a_jnt0, a_jnt1
 
     a_jnt0 = generate_trajectory(t_init, t_end, q0_init, q0_end)
     a_jnt1 = generate_trajectory(t_init, t_end, q1_init, q1_end)
+
+def init_lqr_controller(m, d):
+    global K
+    rho = 1e-2
+    n_dim = 4
+    m_dim = 1
+
+    A,B = linearize(n_dim, m_dim)
+    print(A)
+    print(B)
+
+    Q = np.eye(n_dim)
+    R = rho*np.eye(m_dim)
+    K, _, _ = control.lqr(A, B, Q, R)
+
+def lqr_controller(m, d):
+    x = np.array([d.qpos[0],d.qpos[1],d.qvel[0],d.qvel[1]])
+    u = -K.dot(x)
+    d.ctrl[0] = u[0]
+
+    tau_disturb_mean = 0
+    tau_disturb_dev = 20
+    tau_d0 = np.random.normal(tau_disturb_mean,tau_disturb_dev)
+    tau_d1 = np.random.normal(tau_disturb_mean,0.25*tau_disturb_dev)
+    d.qfrc_applied[0] = tau_d0
+    d.qfrc_applied[1] = tau_d1
 
 def controller(m, d):
     global a_jnt0, a_jnt1
@@ -95,11 +122,67 @@ def generate_trajectory(t0, tf, q0, qf):
 
     return a0, a1, a2, a3
 
+def f(x,u):
+    #x=q0,q1,qdot0,qdot1
+    #u=torque
+
+    d.qpos[0] = x[0]
+    d.qpos[1] = x[1]
+    d.qvel[0] = x[2]
+    d.qvel[1] = x[3]
+
+    d.ctrl[0] = u[0]
+    #d.ctrl[1] = u[1]
+
+    mj.mj_forward(m, d)
+
+    #qddot = inv(M)*(data_ctrl-frc_bias)
+    M = np.zeros((2,2))
+    mj.mj_fullM(m, d, M)
+    invM = np.linalg.inv(M)
+    frc_bias = np.array([d.qfrc_bias[0], d.qfrc_bias[1]])
+    tau = np.array([u[0],0])
+    qddot = np.matmul(invM,np.subtract(tau,frc_bias))
+
+    xdot = np.array([d.qvel[0], d.qvel[1], qddot[0], qddot[1]])
+
+    return xdot
+
+def linearize(n,m):
+    A=np.zeros((n,n))
+    B=np.zeros((n,m))
+    
+    x0 = np.array([0,0,0,0])
+    u0 = np.array([0])
+    xdot0 = f(x0,u0)
+    eps = 1e-2
+
+    for i in range(0, n):
+        x = [0]*n
+        u = u0
+        for j in range(0,n):
+            x[j] = x0[j]
+        x[i] = x[i] + eps
+        xdot = f(x,u)
+        for k in range(0,n):
+            A[k,i] = (xdot[k] - xdot0[k]) / eps
+    
+    for i in range(0, m):
+        x = x0
+        u = [0]*m
+        for j in range(0,m):
+            u[j] = u0[j]
+        u[i] = u[i] + eps
+        xdot = f(x,u)
+        for k in range(0,n):
+            B[k,i] = (xdot[k] - xdot0[k]) / eps
+    
+    return A,B
+
+m = mujoco.MjModel.from_xml_path(XML_PATH)
+d = mujoco.MjData(m)
 
 def main():
-    m = mujoco.MjModel.from_xml_path(XML_PATH)
-    d = mujoco.MjData(m)
-
     camera_name = "my_cam"
     camera_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_CAMERA, camera_name)
 
@@ -108,19 +191,19 @@ def main():
     cam.fixedcamid = camera_id
 
     # Set initial state before launching viewer / registering controller
-    d.qpos[0] = q0_init
+    """d.qpos[0] = q0_init
     d.qpos[1] = q1_init
-    mj.mj_forward(m, d)
+    mj.mj_forward(m, d)"""
 
-    init_controller(m, d)
-    mj.set_mjcb_control(controller)
+    init_lqr_controller(m, d)
+    mj.set_mjcb_control(lqr_controller)
 
     print("Launching MuJoCo Interactive Viewer...")
     print(f"Loading model from: {XML_PATH}")
 
     with mujoco.viewer.launch_passive(m, d) as viewer:
-        viewer.cam.type = cam.type
-        viewer.cam.fixedcamid = cam.fixedcamid
+        """viewer.cam.type = cam.type
+        viewer.cam.fixedcamid = cam.fixedcamid"""
 
         """xs, ys = draw_circle(N=2000, xc=0.75, yc=1, r=0.25)
         x_all = []
@@ -156,7 +239,8 @@ def main():
 
             i += 1
             """
-            if d.time>=t_end:                
+
+            """if d.time>=t_end:                
                 errors_0 = np.subtract(qref0, qact0)
                 errors_1 = np.subtract(qref1, qact1)
 
@@ -176,7 +260,7 @@ def main():
                 plt.savefig("error.png")
                 print("Saved plot to error.png")
                 plt.close()
-                return
+                return"""
             
             mj.mj_step(m, d)
             
